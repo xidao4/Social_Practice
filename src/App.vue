de6b4bcf684a836f9beafeee00c4c2a3ee35489b
superfree <181250003@smail.nju.edu.cn>
2019-06-29 09:52:39 +0800

    add element ui

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
+      <el-button round>element button</el-button> |
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
