c672cb9b012f613903de0e7fa0ab3cbcc90bd864
Wangxi <763098805@qq.com>
2019-09-03 11:46:58 +0800

    调整川派建筑介绍

--- src/views/Architecture.vue
+++ src/views/Architecture.vue
@@ -31,69 +31,69 @@ export default {
     },
     methods:{
         chuan(){
-            this.$alert('皖派建筑的尊贵，在于它优雅了千年的徽派民居。青瓦、白墙是徽派建筑的突出印象。错落有致的马头墙不仅有造型之美，更重要的是它有防火，阻断火灾蔓延的使用功能。', '皖派', {
+            this.$alert('在川派建筑中傣族竹楼、侗族鼓楼、川西吊脚楼最具有鲜明特色。竹楼是一种干栏式建筑，主要用竹子建造，下层高约七八尺，四无遮拦，牛马拴住与柱上。上层进梯出有一露台，用竹篱隔离出主人卧室并兼重要钱物存储式。其余为一敞间。楼中央是一个火塘，日夜燃烧不息。', '川派', {
                 confirmButtonText: '确定',
-                callback: action => {
-                    this.$message({
-                        type: 'info',
-                        message: `action: ${action}`
-                    });
-                }
+                // callback: action => {
+                //     this.$message({
+                //         type: 'info',
+                //         message: `action: ${action}`
+                //     });
+                // }
             })
         },
         jing(){
             this.$alert('京派建筑，在于它历经700多年演变而来的四合院，四合院房屋独立，大到皇宫王府，小到平民住宅，每一处雕饰、每一笔彩绘，都是无价之宝。京派建筑里四合院最为典型，四合院是北京地区乃至华北地区的传统住宅。\n', '京派', {
                 confirmButtonText: '确定',
-                callback: action => {
-                    this.$message({
-                        type: 'info',
-                        message: `action: ${ action }`
-                    });
-                }
+                // callback: action => {
+                //     this.$message({
+                //         type: 'info',
+                //         message: `action: ${ action }`
+                //     });
+                // }
             });
         },
         jin(){
             this.$alert('晋派建筑在很大程度上反应了晋商的品格，稳重、大气、严谨、深沉。斗拱飞檐、彩饰金装、砖瓦磨合、精工细做，它所蕴含的文化与精神是一笔无与伦比的财富。\n', '晋派', {
                 confirmButtonText: '确定',
-                callback: action => {
-                    this.$message({
-                        type: 'info',
-                        message: `action: ${ action }`
-                    });
-                }
+                // callback: action => {
+                //     this.$message({
+                //         type: 'info',
+                //         message: `action: ${ action }`
+                //     });
+                // }
             });
         },
         hui(){
             this.$alert('徽派建筑有“三绝”（民居、祠堂、牌坊）和“三雕”（木雕、石雕、砖雕）。能工巧匠施尽其技，每一处细节每一处雕刻，结构严谨、雕镂精湛。', '徽派', {
                 confirmButtonText: '确定',
-                callback: action => {
-                    this.$message({
-                        type: 'info',
-                        message: `action: ${ action }`
-                    });
-                }
+                // callback: action => {
+                //     this.$message({
+                //         type: 'info',
+                //         message: `action: ${ action }`
+                //     });
+                // }
             });
         },
         min(){
             this.$alert('闽，即福建，闽派民居流行与闽南地区的一种建筑风格，其中“土楼”是最为鲜明的代表。是一种供聚族而居，且具有防御性的民居建筑。\n', '闽派', {
                 confirmButtonText: '确定',
-                callback: action => {
-                    this.$message({
-                        type: 'info',
-                        message: `action: ${ action }`
-                    });
-                }
+                // callback: action => {
+                //     this.$message({
+                //         type: 'info',
+                //         message: `action: ${ action }`
+                //     });
+                // }
             });
         },
         su(){
             this.$alert('苏派建筑，自春秋战国时期人们开始追求，脊角翘高的屋顶，江南风韵的门楼，曲折蜿蜒、藏而不露，饲鸟养鱼、叠石迭景，堪称园林布局的艺术典范。\n', '苏派', {
                 confirmButtonText: '确定',
-                callback: action => {
-                    this.$message({
-                        type: 'info',
-                        message: `action: ${ action }`
-                    });
-                }
+                // callback: action => {
+                //     this.$message({
+                //         type: 'info',
+                //         message: `action: ${ action }`
+                //     });
+                // }
             });
         }
     }
